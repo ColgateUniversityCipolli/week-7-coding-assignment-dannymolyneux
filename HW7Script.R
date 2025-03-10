520fd944ce97cc3fa24bb618a406d0081beac944
dannymolyneux <dmolyneux@colgate.edu>
2025-03-09 22:14:02 -0400

    Question 2

--- HW7Script.R
+++ HW7Script.R
@@ -26,4 +26,31 @@ pois.prob <- function(x, lambda, type){
   return(prob)
 }
 
+#Question 2
+beta.prob <- function(x, alpha, beta, type){
+  if(type == "="){
+    #For a continuous distribution, probability is zero at any given point
+    prob = 0
+  }
+  else if(type == "!="){
+    #The compliment of "="
+    prob = 1
+  }
+  else if(type == "<" | type == "<="){
+    #These two operators are equivalent for continuous distributions
+    prob = pbeta(x, alpha, beta)
+  }
+  else if(type == ">" | type == ">="){
+    #These two operators are equivalent for continuous distributions
+    prob = 1 - pbeta(x, alpha, beta)
+  }
+  else{
+    print("Invalid input. Please enter a valid operator for the 'type' parameter.")
+    return(0)
+  }
+  return(prob)
+}
+
+
+
 
